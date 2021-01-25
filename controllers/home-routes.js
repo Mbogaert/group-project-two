cd938d7e5ad8012fe102fc20418180be97a1d52c
James Costello <jmc@Jamess-MacBook-Pro.local>
2021-01-24 19:55:02 -0700

    update home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,36 +3,15 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  Post.findAll({
-    attributes: [
-      'id',
-      'title',
-      'created_at',
-      'content'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
