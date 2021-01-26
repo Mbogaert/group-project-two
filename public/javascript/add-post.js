38dfdfd8f4bcd0b9cfe96e6248f165867ef4b4d7
Mathew B <bogaert@email.arizona.edu>
2021-01-25 20:11:23 -0700

    put add-dog and new-post HTML and front-end js together

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,12 +1,21 @@
 async function newFormHandler(event) {
     event.preventDefault();
-  
+
+    const dog_name = document.querySelector('#dog-name').value.trim();
+    const energy = document.querySelector('input[name="dog-energy"]').value;
+    const vaccine = document.querySelector('input[name="dog-vacc"]').value;
+    const human_interaction = document.querySelector('input[name="interaction"]').value;
     const title = document.querySelector('input[name="post-title"]').value;
     const content = document.querySelector('input[name="post-content"]').value;
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
+        dog_name,
+        breed,
+        energy,
+        vaccine,
+        human_interaction,
         title,
         content
       }),
@@ -16,7 +25,7 @@ async function newFormHandler(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/dashboard/results');
     } else {
       alert(response.statusText);
     }
