9e89fba7f7d01f374db1f91448a093f0ef47c7dc
James Costello <jmc@Jamess-MacBook-Pro.local>
2021-01-25 22:41:26 -0700

    dash updates and front end edits

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,10 +9,6 @@ router.get("/new-post", (req, res) => {
   res.render('new-post');
 });
 
-router.get("/add-dog", (req, res) => {
-  res.render('add-dog');
-});
-
 router.get("/results", (req, res) => {
   res.render('results');
 });
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,34 +1,35 @@
 async function newFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const dog_name = document.querySelector('#dog-name').value.trim();
-    const energy = document.querySelector('input[name="dog-energy"]').value;
-    const vaccine = document.querySelector('input[name="dog-vacc"]').value;
-    const human_interaction = document.querySelector('input[name="interaction"]').value;
-    const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="post-content"]').value;
-  
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        dog_name,
-        breed,
-        energy,
-        vaccine,
-        human_interaction,
-        title,
-        content
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/results');
-    } else {
-      alert(response.statusText);
+  const dog_name = document.querySelector('#dog-name').value.trim();
+  const breed = document.querySelector('#dog-breed').value.trim();
+  const energy = document.querySelector('input[name="dog-energy"]').value;
+  const vaccine = document.querySelector('input[name="dog-vacc"]').value;
+  const human_interaction = document.querySelector('input[name="interaction"]').value;
+  const title = document.querySelector('input[name="post-title"]').value;
+  const content = document.querySelector('input[name="post-content"]').value;
+
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      dog_name,
+      breed,
+      energy,
+      vaccine,
+      human_interaction,
+      title,
+      content
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/results');
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
