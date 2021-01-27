4aa9eaa17ae90ff31c8937b252c68e551bb0439f
Mathew B <bogaert@email.arizona.edu>
2021-01-26 18:50:36 -0700

    radio buttons selection working, response is not

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,42 +1,57 @@
 async function newFormHandler(event) {
   event.preventDefault();
-
+  
   const dog_name = document.querySelector('#dog-name').value.trim();
   const breed = document.querySelector('#dog-breed').value.trim();
-  let energy = document.querySelectorAll('input[name="dog-energy"]');
-  if (document.getElementById('high-energy').checked) {
-    energy = document.getElementById('high-energy').value;
-  } else if (document.getElementById('in-between').checked) {
-    energy = document.getElementById('in-between').value;
-  } else if (document.getElementById('mellow').checked) {
-    energy = document.getElementById('mellow').value; } 
-  const vaccine = document.querySelectorAll('input[name="dog-vacc"]').value;
-  const human_interaction = document.querySelectorAll('input[name="interaction"]').value;
   const title = document.querySelector('input[name="post-title"]').value;
   const content = document.querySelector('input[name="post-content"]').value;
+  
+  const allEnergy = document.querySelectorAll('input[name="dog-energy"]');
+  let energy;
+    for (const rb of allEnergy) {
+      if (rb.checked) {
+        energy = rb.value;
+      }
+    };
 
-  const response = await fetch(`/api/posts`, energy, {
-    method: 'POST',
-    body: JSON.stringify({
-      dog_name,
-      breed,
-      energy,
-      vaccine,
-      human_interaction,
-      title,
-      content
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+  const allVaccine = document.querySelectorAll('input[name="dog-vacc"]');
+  let vaccine;
+    for (const rb of allVaccine) {
+      if (rb.checked) {
+        vaccine = rb.value;
+      }
+    };
 
-  if (response.ok) {
-    // document.location.replace('/dashboard/results');
-    console.log(dog_name, energy, energyChecked);
-  } else {
-    alert(response.statusText);
-  }
+  const allHuman_interaction = document.querySelectorAll('input[name="interaction"]');
+  let human_interaction;
+    for (const rb of allHuman_interaction) {
+      if (rb.checked) {
+        human_interaction = rb.value;
+      }
+    };
+
+  // const response = await fetch(`/api/posts`, {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     dog_name,
+  //     breed,
+  //     energy,
+  //     vaccine,
+  //     human_interaction,
+  //     title,
+  //     content
+  //   }),
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // });
+
+  // if (response.ok) {
+  //   document.location.replace('/dashboard/results');
+    console.log(dog_name, energy, vaccine, human_interaction);
+  // } else {
+  //   alert(response.statusText);
+  // }
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
