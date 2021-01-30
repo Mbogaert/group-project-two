ac769f6f1682a92cead81881e13a777c61fad5aa
Mathew B <bogaert@email.arizona.edu>
2021-01-30 11:21:03 -0700

    made it so when logged In when clicking Plenty of Pups it redirects to dashboard instead of home

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('./dashboard');
+    return;
+  }
+  
   res.render('homepage');
 });
 
